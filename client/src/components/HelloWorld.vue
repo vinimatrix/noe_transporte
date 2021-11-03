6775335d4d5ecc67eae09c8c22a90face64d1709
vinicio mendez <mvini_man@hotmail.com>
2021-11-03 16:02:26 -0400

    inapa

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,10 +1,5 @@
 <template>
-<form>
-  <label for="name">email</label>
-  <input type="text" name="name">
-  <label for="name">password</label>
-  <input type="text" name="password">
-</form>
+<h1>NOE TRANSPORTE</h1>
 
 </template>
 
---
+++ client/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <form>
+  <label for="name">email</label>
+  <input type="text" name="name">
+  <label for="name">password</label>
+  <input type="text" name="password">
+</form>
+</template>
+<script>
+
+export default ({
+    name:'Login'
+})
+</script>
