04828ae1fb91102a71784e486924dd4744b950ba
vinicio mendez <mvini_man@hotmail.com>
2021-11-17 08:41:05 -0400

    preparacion de los modulos

--- client2/src/app/app-routing.module.ts
+++ client2/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('./reportes/reportes.module').then(m => m.ReportesModule)
+  },
+  {
+    path:'reportes',
+    loadChildren:()=>import('./reportes/reportes.module').then(m => m.ReportesModule)
+  },
+  {
+    path:'conductores',
+    loadChildren:()=>import('./conductores/conductores.module').then(m => m.ConductoresModule)
+  },
+  {
+    path:'pasajeros',
+    loadChildren:()=>import('./pasajeros/pasajeros.module').then(m => m.PasajerosModule)
+  },
+  {
+    path:'rutas',
+    loadChildren:()=>import('./rutas/rutas.module').then(m => m.RutasModule)
+  }
+  ,
+  {
+    path:'viajes',
+    loadChildren:()=>import('./viajes/viajes.module').then(m => m.ViajesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
